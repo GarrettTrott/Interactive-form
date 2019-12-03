e0df7d8dc5ac0ecc64d5849af16330fe583abb7d
Garrett Trott <garretttrott@gmail.com>
2019-12-03 13:56:40 -0800

    Focused on Name input,  Created Other Input & hid

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
+// Focus on first input - name field.
+$("#name").focus();
 
-
-
-
-    
-}
+//Hide other job role input
+$("#other-title").hide();
