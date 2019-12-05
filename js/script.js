9a3ff56785d826b9d5ef10b84e5ee94cb60d1d35
Garrett Trott <garretttrott@gmail.com>
2019-12-05 01:01:01 -0800

    adds payment selection hide & show

--- js/script.js
+++ js/script.js
@@ -54,10 +54,38 @@ const $totalHeader = $(
 $totalHeader.show();
 $(".activities").append($totalHeader);
 
-$('[type="checkbox"]').change(function() {
+$('input[type="checkbox"]').change(function() {
   let totalCost = 0;
+  const $time = $(this).attr("data-day-and-time");
   $("input[type=checkbox]:checked").each(function() {
     totalCost += parseInt($(this).attr("data-cost"));
   });
+
+  $("input[type=checkbox]").each(function(index, element) {
+    // element == this
+  });
+
   $("#total").text(totalCost);
+});
+
+// payment option show / hide
+
+$("option[value='select method']").hide();
+$("option[value='credit card']").attr("selected", true);
+$("#paypal").hide();
+$("#bitcoin").hide();
+$("#payment").change(function() {
+  if ($(this).val() === "paypal") {
+    $("#credit-card").hide();
+    $("#paypal").show();
+    $("#bitcoin").hide();
+  } else if ($(this).val() === "bitcoin") {
+    $("#credit-card").hide();
+    $("#paypal").hide();
+    $("#bitcoin").show();
+  } else {
+    $("#credit-card").show();
+    $("#paypal").hide();
+    $("#bitcoin").hide();
+  }
 });
