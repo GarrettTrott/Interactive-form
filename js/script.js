ae022a5d5b0207eac4ada2a0b408ad96314c792a
Garrett Trott <garretttrott@gmail.com>
2019-12-04 22:31:30 -0800

    add total price calculator

--- js/script.js
+++ js/script.js
@@ -32,5 +32,24 @@ $("#design").change(function() {
     $(
       '#color option[value="tomato"], option[value="steelblue"], option[value="dimgrey"]'
     ).show();
+  } else {
+    $("#color option").each(function() {
+      $(this).hide();
+    });
   }
+});
+
+// add activities hide and show
+const $totalHeader = $(
+  '<p>Your total price for activities is: <strong>$<span id="total">0</span><strong></p>'
+);
+$totalHeader.show();
+$(".activities").append($totalHeader);
+
+$('[type="checkbox"]').change(function() {
+  let totalCost = 0;
+  $("input[type=checkbox]:checked").each(function() {
+    totalCost += parseInt($(this).attr("data-cost"));
+  });
+  $("#total").text(totalCost);
 });
