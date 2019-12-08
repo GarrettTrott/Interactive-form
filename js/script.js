41c95700128a6ca7506b6f1502ffe03237b35b0a
Garrett Trott <garretttrott@gmail.com>
2019-12-07 22:41:48 -0800

    adds submit verification and message

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $("#name").focus();
 
 // Hide other job role input show when other is selected
 $("#other-title").hide();
-
 $("#title").change(function() {
   if ($(this).val() === "other") {
     $("#other-title").show();
@@ -12,18 +11,19 @@ $("#title").change(function() {
   }
 });
 
-// Hide all color options
-$("#color option").each(function() {
-  $(this).hide();
-});
-
 // Create "select theme" option placeholder and append to colors dropdown
 const $selectTheme = $(
   '<option value="noSelection" selected>Please select a T-shirt theme</option>'
 );
 $("#color").prepend($selectTheme);
 
-// hide/show correct color options for design
+// Hide all color options
+
+$("#color option").each(function() {
+  $(this).hide();
+});
+
+// hide/show correct color options for design selected
 
 $("#color").hide();
 $("#color")
@@ -65,6 +65,7 @@ $("#design").change(function() {
 });
 
 // add activities hide and show
+
 const $totalHeader = $(
   '<p>Your total price for activities is: <strong>$<span id="total">0</span><strong></p>'
 );
@@ -74,12 +75,13 @@ $(".activities").append($totalHeader);
 $('input[type="checkbox"]').change(function() {
   let totalCost = 0;
   const $time = $(this).attr("data-day-and-time");
-  $("input[type=checkbox]:checked").each(function() {
-    totalCost += parseInt($(this).attr("data-cost"));
-  });
 
   $("input[type=checkbox]").each(function(index, element) {
     // element == this
+  });
+
+  $("input[type=checkbox]:checked").each(function() {
+    totalCost += parseInt($(this).attr("data-cost"));
   });
 
   $("#total").text(totalCost);
@@ -104,5 +106,204 @@ $("#payment").change(function() {
     $("#credit-card").show();
     $("#paypal").hide();
     $("#bitcoin").hide();
+  }
+});
+
+// add validation spans to input labels//
+
+const $nameValidation = $("<span></span>");
+const $emailValidation = $("<span></span>");
+const $activitiesValidation = $("<span></span>");
+const $creditValidation = $("<span></span>");
+const $zipValidation = $("<span></span>");
+const $cvvValidation = $("<span></span>");
+
+// append validation spans to labels
+$("#name")
+  .prev()
+  .append($nameValidation);
+$("#mail")
+  .prev()
+  .append($emailValidation);
+$($activitiesValidation).insertAfter($("legend").eq(2));
+$("#cc-num")
+  .prev()
+  .append($creditValidation);
+$("#zip")
+  .prev()
+  .append($zipValidation);
+$("#cvv")
+  .prev()
+  .append($cvvValidation);
+
+// form Validation functions
+
+function isValidName(name) {
+  return /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/.test(name);
+}
+
+function isValidEmail(email) {
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
+    email
+  );
+}
+
+function activitiesAreSelected() {
+  return $("input[type=checkbox]:checked").length >= 1;
+}
+
+function isValidCardNumber(cardNumber) {
+  let cardType;
+  const visa = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/.test(cardNumber);
+  const mastercard = /^(?:5[1-5][0-9]{14})$/.test(cardNumber);
+  const amex = /^(?:3[47][0-9]{13})$/.test(cardNumber);
+  const discover = /^(?:6(?:011|5[0-9][0-9])[0-9]{12})$/.test(cardNumber);
+
+  if (visa === true) {
+    cardType = "Visa";
+  } else if (mastercard === true) {
+    cardType = "Mastercard";
+  } else if (amex === true) {
+    cardType = "Amex";
+  } else if (discover === true) {
+    cardType = "Discover";
+  } else {
+    cardType = false;
+  }
+
+  return cardType;
+}
+
+function isValidZipCode(zipcode) {
+  return /^\d{5}$|^\d{5}-\d{4}$/.test(zipcode);
+}
+
+function isValidCVV(cvv) {
+  return /^[0-9]{3,4}$/.test(cvv);
+}
+
+function 
+
+function $colorValidateInput(
+  $input,
+  validatorFunc,
+  invalidMessage = " Invalid",
+  validMessage = ""
+) {
+  if (validatorFunc($input.val())) {
+    $input.removeClass("invalidBorder");
+    $input.addClass("isValidBorder");
+    $input
+      .prev()
+      .children()
+      .removeClass("invalidLabel");
+    $input
+      .prev()
+      .children()
+      .addClass("isValidLabel");
+    $input
+      .prev()
+      .children()
+      .text(validMessage);
+  } else {
+    $input.removeClass("isValidBorder");
+    $input.addClass("invalidBorder");
+    $input
+      .prev()
+      .children()
+      .removeClass("isValidLabel");
+    $input
+      .prev()
+      .children()
+      .addClass("invalidLabel");
+    $input
+      .prev()
+      .children()
+      .text(invalidMessage);
+  }
+}
+
+// Keyup validation functions
+
+$("#name").keyup(function() {
+  $colorValidateInput($(this), isValidName, " Please enter your name");
+});
+
+$("#mail").keyup(function() {
+  $colorValidateInput(
+    $(this),
+    isValidEmail,
+    " Please enter your email address"
+  );
+});
+
+$("#cc-num").keyup(function() {
+  $colorValidateInput(
+    $(this),
+    isValidCardNumber,
+    "Enter a valid card number",
+    isValidCardNumber($(this).val())
+  );
+});
+
+$("#zip").keyup(function() {
+  $colorValidateInput($(this), isValidZipCode);
+});
+
+$("#cvv").keyup(function() {
+  $colorValidateInput($(this), isValidCVV);
+});
+
+const $submitMessage = $('<div id="submitMessage"><div>');
+$($submitMessage).insertBefore("button[type=submit]");
+$submitMessage.css("text-align", "center");
+// submit form
+
+$("form").submit(function(e) {
+  e.preventDefault();
+
+  function successMessage() {
+    $submitMessage.css("color", "#34a853");
+    $("#submitMessage").text("Registered for Full Stack Comf! See you soon!");
+  }
+
+  // color validate before submit //
+  $colorValidateInput($("#name"), isValidName, " Please enter a valid name");
+  $colorValidateInput(
+    $("#mail"),
+    isValidEmail,
+    " Please enter a valid email address"
+  );
+  $colorValidateInput(
+    $("#cc-num"),
+    isValidCardNumber,
+    "Enter a valid card number",
+    isValidCardNumber($(this).val())
+  );
+  $colorValidateInput($("#zip"), isValidZipCode);
+  $colorValidateInput($("#cvv"), isValidCVV);
+  
+  // Validate all required fields //
+
+  if (
+    isValidName($("#name").val()) &&
+    isValidEmail($("#mail").val()) &&
+    activitiesAreSelected() &&
+    $("#payment").val() !== "credit card"
+  ) {
+    successMessage();
+  } else if (
+    isValidName($("#name").val()) &&
+    isValidEmail($("#mail").val()) &&
+    activitiesAreSelected() &&
+    $("#payment").val() === "credit card" &&
+    isValidCardNumber($("#cc-num").val()) &&
+    isValidZipCode($("#zip").val()) &&
+    isValidCVV($("#cvv").val())
+  ) {
+    successMessage();
+  } else {
+    $submitMessage.css("color", "#e01a0f");
+    $("#submitMessage").text("Please fill out the required fields above!");
   }
 });
